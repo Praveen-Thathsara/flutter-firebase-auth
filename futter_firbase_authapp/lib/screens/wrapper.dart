0d5006925babb791d51b55a73271f4a56e1089c8
Praveen Thathsara <praveenstudy823@gmail.com>
2025-11-23 01:12:30 +0530

    create the file structure and checking the basic logic with wrapper

---
+++ futter_firbase_authapp/lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:futter_firbase_authapp/screens/authentication/authenticate.dart';
+import 'package:futter_firbase_authapp/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Authenticate());
+  }
+}
