59a7a5990f1b7a3607d05d586cd27e7e754bb4a0
Praveen Thathsara <praveenstudy823@gmail.com>
2025-11-23 00:55:14 +0530

    set up firebase to the flutter

--- futter_firbase_authapp/android/app/build.gradle.kts
+++ futter_firbase_authapp/android/app/build.gradle.kts
@@ -3,12 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.futter_firbase_authapp"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34        // replace flutter.compileSdkVersion
+    ndkVersion = "25.2.9519653"  // optional, replace flutter.ndkVersion or remove
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -16,24 +17,19 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"  // simpler string
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.futter_firbase_authapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = flutter.minSdkVersion          // replace flutter.minSdkVersion
+        targetSdk = 34       // replace flutter.targetSdkVersion
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
